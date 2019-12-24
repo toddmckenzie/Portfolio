68fc2d91773f98d4655607e03ea4aa2c31fe3732
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-12-23 22:46:17 -0600

    roughed in header

--- port/src/App.js
+++ port/src/App.js
@@ -6,17 +6,19 @@ function App() {
   return (
     <div class="container">
       <header class="header">
-          <a class="first">About</a>
+          <a class="first">Home</a>
+          <a>About</a>
           <a>Projects</a>
+          <a>Skills</a>
           <a class="last">Contact</a>
       </header>
-      <div class="about-div">
+        <div class="about">
               <h2>About Me</h2>
               <p>I grew up in a small town in southeast Kansas where sports were my life.  I continued to play sports and wrestled a couple of years in college, but had little idea or direction with what I wanted to do with my life.  I bounced around a little before ending up finished a bachelors degree in Human Biology from Cleveland University Kansas City.  Shortly after graduating I moved to St. Louis to be closer to my girlfriend, who is now my wife.  I started working for a chemical plant as a contracted lab technician.  When that job ended I took an operator position with the company.  I ended up working there for a little over 12 years.  During this period my wife and I had 2 sets of twins.</p>  
               <p>Leaving my job was a difficult one because it paid extremely well, I had people counting on me to make money, and my wife was not working since she was taking care of our 4 children who are 5 and 3.  My wife agreed to work weekends to allow me to pursue my passion.  The reason I left was because I developed a love for coding.  I started learning python on the side of work and family and I became hooked.  For once in my life I had found an opportunity to do something I enjoyed.  I love learning and making new apps that users love.  I love being stuck on a problem and waking up in the middle of the night with the answer.  I am obsessed with code and want to grow to become the best developer I can obtain.</p>
               <p>I left work to attend Lambda School.  Lambda school is a 30 plus week fulltime fullstack and computer science school.  At Lambda I learned HTML, CSS, LESS, SASS, responsive web development, Javascript, React, Redux, Node.js, Jest, Express, SQL.  In the computer science portion I learned more Python, algorithms, data structures, computer architecture and more.  I learned how to problem solve better, how to read the DOCs, and how to google a little better.  I spent many days working 12 hours a day coding, watching tutotrials through Lambda school and youtube, and solving problems.  I also continued to work on weekends in order to achieve my goals that I have for myself.</p>
           </div>
-        </div>
+      </div>
   );
 }
 
