31d62d26bdad7deb972dd5729a6d31f693693d2a
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-12-30 11:39:45 -0600

    working on contact page

--- port/src/components/contact.js
+++ port/src/components/contact.js
@@ -6,7 +6,11 @@ import code from '../img/code-reg.png';
 const Contact = () => {
     return (
         <div className="contact" style={{backgroundImage: `url(${code})`}}>
+            <p className="phone">Phone: 314-605-0504</p>
+            <p className="email">Email: toddmckenzie@hotmail.com</p>
+            <div className="box">
 
+            </div>
         </div>
     )
 }
