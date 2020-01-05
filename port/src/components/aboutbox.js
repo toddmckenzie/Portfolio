63caea15e407d0cf603b83dc58b4a188839d425c
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2020-01-04 21:24:56 -0600

    working on bottom of home page

---
+++ port/src/components/aboutbox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './box.css'
+
+const AboutBox = (props) => {
+    return (
+       <div className="halfA">
+           <div className="circle">
+               <p className="ach">About Me</p>
+           </div>
+       </div>
+    )
+}
+
+export default AboutBox;
+
---
+++ port/src/components/contactbox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './box.css'
+
+
+const ContactBox = () => {
+    return (
+        <div className="halfC">
+            <div className="circle">
+                <p className="ach">Contact</p>
+            </div>
+        </div>
+    )
+}
+
+export default ContactBox;
