55442ca784676084faa549c9d15adc460ccd7c49
Todd McKenzie <toddmckenzie@hotmail.com>
2021-07-17 19:46:18 -0400

    added build file

---
+++ port/build/precache-manifest.8da45e32bbaf81f002d77491df1748a9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf99fe4bb8b5828e5aa45a07ea68ce46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c8e8cef79a50195ea7e",
+    "url": "/static/css/main.1986acef.chunk.css"
+  },
+  {
+    "revision": "af1854aec3bf8673598e",
+    "url": "/static/js/2.8b5dc02e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.8b5dc02e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c8e8cef79a50195ea7e",
+    "url": "/static/js/main.521be078.chunk.js"
+  },
+  {
+    "revision": "ed6fba4bafcfed78ba7d",
+    "url": "/static/js/runtime-main.dbc35a70.js"
+  },
+  {
+    "revision": "c51b75ec47b1835d08c5362ae1027293",
+    "url": "/static/media/cloudstands.c51b75ec.png"
+  },
+  {
+    "revision": "985a93b4e2faeaa7f85386c4858a5af7",
+    "url": "/static/media/code-un.985a93b4.jpg"
+  },
+  {
+    "revision": "41fd38cd9b970f17f9e1bc3a2ff9ca2b",
+    "url": "/static/media/db.41fd38cd.png"
+  },
+  {
+    "revision": "f7423ecb148d9c244f2e0f891f013e9f",
+    "url": "/static/media/didact.f7423ecb.png"
+  },
+  {
+    "revision": "ff40c1650431c79606a35e16b6838b37",
+    "url": "/static/media/family.ff40c165.png"
+  },
+  {
+    "revision": "6feeb8f7cdf8636973b23a68712ff208",
+    "url": "/static/media/github.6feeb8f7.svg"
+  },
+  {
+    "revision": "d89bcacbe3ff82ec4a253ab9a994ffc9",
+    "url": "/static/media/home.d89bcacb.png"
+  },
+  {
+    "revision": "94025d95f969a661b109aab906d63047",
+    "url": "/static/media/linkedin.94025d95.svg"
+  },
+  {
+    "revision": "adb782a22a001aab94947c9740181011",
+    "url": "/static/media/man.adb782a2.png"
+  },
+  {
+    "revision": "ce5acf1dd1ad90bcb100da9336d68fa7",
+    "url": "/static/media/phone.ce5acf1d.png"
+  },
+  {
+    "revision": "cab4e23fcbc403cfc72314f245e4be12",
+    "url": "/static/media/todd-hat.cab4e23f.jpg"
+  },
+  {
+    "revision": "f39e3b890bb6a0ee6353ab1b14671900",
+    "url": "/static/media/todd.f39e3b89.jpg"
+  },
+  {
+    "revision": "a3e6256f0300acc875ef4d33e040f934",
+    "url": "/static/media/tweet-better.a3e6256f.png"
+  }
+]);
