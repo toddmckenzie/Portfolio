f95e9d8c0c8639bef2b6da63be25db0ea8163a21
Todd McKenzie <toddmckenzie@hotmail.com>
2021-07-17 19:13:05 -0400

    test

--- port/src/components/footer.js
+++ port/src/components/footer.js
@@ -4,9 +4,10 @@ import github from '../img/github.svg';
 import linkedin from '../img/linkedin.svg';
 
 const Footer = () => {
+    //test
     return (
         <div className="footer">
-            <p className="copy">C 2020 Todd McKenzie</p>
+            <p className="copy">C 2021 Todd McKenzie</p>
             <a href="https://github.com/toddmckenzie" target="_blank">
                 <img src={github} className="icons" />
             </a>
