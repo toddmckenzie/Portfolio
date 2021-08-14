59364725b4872e16c7d118636e26328ae94833ac
Todd McKenzie <toddmckenzie@hotmail.com>
2021-08-14 18:07:54 -0400

    redoing port to be easier to update and add info

---
+++ src/components/aboutData.js
@@ -0,0 +1,37 @@
+import family from '../img/family.png';
+import todd from '../img/todd.jpg';
+import hat from '../img/todd-hat.jpg';
+import fsw from '../img/fsw-buc-logo.png';
+
+const aboutMe = [
+    {
+        id: 1,
+        img: todd,
+        altImg: "Picture of Todd McKenzie",
+        paragraph: 'I grew up in a small town in southeast Kansas where sports were my life. I played basketball, football, baseball, tennis, soccer, cross country and even wrestled on the college level. Following my passion for human physicality and athleticism, I completed a bachelors degree in Human Biology from Cleveland University Kansas City. Shortly after graduating I moved to be closer to my girlfriend, who is now my wife, in St.Louis, MO. For 12 years, I worked for a chemical plant as a contracted Quality Control lab technician and later an Operator. During this time, my wife and I had 2 sets of twins (yes, you read that right, 4 children!).'
+
+
+    },
+    {
+        id: 2,
+        img: family,
+        altImg: "Picture of Todd's family walking",
+        paragraph: 'In early 2019, I left work to attend Lambda School, a 30+ week long Full Stack Web Development and Computer Science school. At Lambda I learned HTML/CSS, LESS, SASS, Responsive Web Development, Javascript, React, Redux, Node.js, Jest, Express, and SQL. In the Computer Science focus, I mastered Python and got deep into algorithms, data structures, computer architecture and more. I learned how to efficiently problem solve and troubleshoot code, how to read the DOCs, and how to google like a developer. I spent many days coding for 12 hours straight, pausing only to watching tutorials, and help my peers troubleshoot their code. Throughout it all, I juggled a family and husband duties.'
+
+    },
+    {
+        id: 3,
+        img: hat,
+        altImg: "Todd in Lambda School hat",
+        paragraph: 'My journey through Lambda was challenging, but resulted in a deep love for coding. For once in my life, I can truly say I have found an opportunity to pursue something I enjoy in my career. I love learning and making new apps that users love. I love being stuck on a problem and waking up in the middle of the night with the answer. In short, I am obsessed with code and aim to continue to develop into the best developer I can be.'
+
+    },
+    {
+        id: 4,
+        img: fsw,
+        altImg: 'Florida Southwestern State College Logo',
+        paragraph: 'In November 2020 I took a position in Ft. Myers, FL as a Web Applications Developer for Florida SouthWestern State College.  At FSW I have had the opportunity learn new languages (PHP) and other frameworks (Code Igniter and Laravel).  I have had the chance to work with docker and am currently learning kubernetes.  The opportunity has allowed me to broaden my development experience and work with an experienced developers.'
+    }
+];
+
+export default aboutMe;
