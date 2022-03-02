060c9ecb99baaeac7b92f7948b85aca7182838d3
Todd McKenzie <toddmckenzie@hotmail.com>
2022-03-02 15:44:48 -0500

    changed links on projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,14 +11,14 @@ const Projects = () => {
         <div className="projects">
             <div className="dorothy">
                 <div className="words bunker-words">
-                    <a href="https://expo.io/@toddmckenzie/dorothysbunker" target="_blank" className="db-heading"><h1 className="project-heading db-heading">Dorothy's Bunker</h1></a>
+                    <a href="https://expo.dev/@toddmckenzie/DorothysBunker" target="_blank" className="db-heading"><h1 className="project-heading db-heading">Dorothy's Bunker</h1></a>
                     <h3 className="role">Full Stack Developer</h3>
                     <p>Dorothy's Bunker is a solo-built project using React native and Redux on the front end and Node.js with express framework on the back end.  To create this mobile app, I started by gathering storm locations from various sources such as county government sites and local new stations. Python was then used to fetch the latitude and longitude from the Google Geocoding API to build my seed data for the back end.</p>
                     <p>
                     The ultimate goal was so users could find a nearby storm shelter when severe weather is approaching. Users can also pull open a profile for their nearby shelter and comment on the status of the shelter.
                     </p>
                     <p>
-                    This mobile app was my first time working with React Native but it was a simple transition as it is so similar to ReactJs. You can access the app with the expo app at <a className="db-heading anchorT" href="https://expo.io/@toddmckenzie/dorothysbunker" target="_blank" rel="noreferrer">expo.io/@toddmckenzie/dorothysbunker</a>.
+                    This mobile app was my first time working with React Native but it was a simple transition as it is so similar to ReactJs. You can access the app with the expo app at <a className="db-heading anchorT" href="https://expo.dev/@toddmckenzie/DorothysBunker" target="_blank" rel="noreferrer">https://expo.dev/@toddmckenzie/DorothysBunker</a>.
                     </p>
                     <p>Github Code: <a href="https://github.com/SheltersBunkers/Frontend" className="db-heading" target="_blank" rel="noreferrer" style={{fontSize: "26px", textDecoration: "underline"}}>Front End</a></p>
                     <p>Github Code: <a href="https://github.com/toddmckenzie/Maps-backend" className="db-heading" target="_blank"  rel="noreferrer" style={{fontSize: "26px", textDecoration: "underline"}}>Back End</a></p>
@@ -28,7 +28,7 @@ const Projects = () => {
                 <div className="cloud-div">
                 <img src={cloudstands} className="cloud" />
                     <div className="words">
-                        <a href="https://www.cloudstands.com/landing" target="_blank"><h1 className="project-heading cloud-heading">Cloud Stands</h1></a>
+                        <a><h1 className="project-heading cloud-heading">Cloud Stands</h1></a>
                         <h3 className="role">Full Stack Developer</h3>
                         <p>Cloudstands was a 8 week Lambda Labs project with a team of 6 other developers, a UX designer, and a Team Lead. The first 2 weeks consisted of planning our feature canvases and the tech stack we would use. The next 6 weeks were spent coding to make a usable product.</p>
                         <p>Cloudstands is a website for small market owners and vendors to connect with each other. We built a site where the owner could put in the amount of booths the market had, the cost of the booths, and the days the market was open. The vendor could then access the site to rent booths from any market they wish to do business. The vendor could also view the other booths that were scheduled to be there that day in order to avoid competing in the same market with similar businesses.
@@ -39,7 +39,7 @@ const Projects = () => {
                 </div>
                 <div className="didact-div">
                     <div className="words didact-words">
-                        <a href="https://didactlms.com/landing" target="_blank"><h1 className="project-heading didact-heading">Didact</h1></a>
+                        <a><h1 className="project-heading didact-heading">Didact</h1></a>
                         <h3 className="role">Team Lead</h3>
                         <p>Didact was a Lambda Labs project that I managed as Team Lead. I had a great group of 5 developers and 1 UX designer on my team.</p>
                         <p>
