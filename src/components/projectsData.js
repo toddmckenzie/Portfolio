9f0c0e8f15dc4a474a264689a0c6251aaab63f0d
Todd McKenzie <toddmckenzie@hotmail.com>
2022-03-02 15:42:14 -0500

    changed link for mobile app

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -11,7 +11,7 @@ const my_projects = [
     color: '##3366CC',
     project_image: db,
     about: ["Dorothy's Bunker is a solo-built project using React native and Redux on the front end and Node.js with express framework on the back end.  To create this mobile app, I started by gathering storm locations from various sources such as county government sites and local new stations. Python was then used to fetch the latitude and longitude from the Google Geocoding API to build my seed data for the back end.", "The ultimate goal was so users could find a nearby storm shelter when severe weather is approaching. Users can also pull open a profile for their nearby shelter and comment on the status of the shelter.", "This mobile app was my first time working with React Native but it was a simple transition as it is so similar to ReactJs. You can access the app with the expo app at", "This mobile app was my first time working with React Native but it was a simple transition as it is so similar to ReactJs. You can access the app with the expo app at"],
-    link: "https://expo.io/@toddmckenzie/dorothysbunker",
+    link: "https://expo.dev/@toddmckenzie/DorothysBunker",
     github_front: "https://github.com/SheltersBunkers/Frontend",
     github_back: "https://github.com/toddmckenzie/Maps-backend"
 },
